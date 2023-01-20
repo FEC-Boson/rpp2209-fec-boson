114331e583da40cfd9cf5262d50f65b3f6174c20
HarryMeng <harry.mld.2010@gmail.com>
2023-01-19 23:53:46 -0800

    Update CSS file; Add a sorting portion; Add two buttons; npm install dropdown;

--- client/src/components/Ratings/ratingComponents/ReviewList.jsx
+++ client/src/components/Ratings/ratingComponents/ReviewList.jsx
@@ -2,12 +2,24 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import ReviewTile from './ReviewTile.jsx';
+import Dropdown from 'react-dropdown';
 
 var ReviewList = (props) => {
+
+  // var options = ['Relevant', 'Helpful', 'Newest'];
 
   // Return a list of reviews for a particular product
   return (
     <div>
+      <div>Sorting</div>
+      <div> 248 reviews, sorted by relevant &nabla;</div>
+      <br></br>
+      {/* <Dropdown options={options} defaultValue = {options[0]} value = {options[0]}></Dropdown> */}
+      {/* <select>
+          <option value='Relevant'>Relevant</option>
+          <option value='Helpful'>Helpful</option>
+          <option value='Newest'>Newest</option>
+        </select> */}
       <div>Review List</div>
       {props.reviewsList.results.map((result, i) => {
         return (
@@ -17,6 +29,8 @@ var ReviewList = (props) => {
           </div>
         )
       })}
+      <button>MORE REVIEWS</button>
+      <button>ADD A REVIEW + </button>
     </div>
   )
 }
